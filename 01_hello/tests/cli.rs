62d326ae966b8c4dc3da21c5b1a949929617f3dd
Marvin Hsu <44745544+marvin-hsu@users.noreply.github.com>
2023-04-20 14:40:52 +0000

    lab01_hello

---
+++ 01_hello/tests/cli.rs
@@ -0,0 +1,26 @@
+use std::process::Command;
+
+use assert_cmd::prelude::{CommandCargoExt, OutputAssertExt};
+
+#[test]
+fn runs(){
+    let mut cmd = Command::cargo_bin("hello").unwrap();
+    cmd.assert().success().stdout("Hello, world!\n");
+}
+
+#[test]
+fn true_ok(){
+    let mut cmd = Command::cargo_bin("true").unwrap();
+    cmd.assert().success();
+}
+
+#[test]
+fn false_not_ok(){
+    let mut cmd = Command::cargo_bin("false").unwrap();
+    cmd.assert().failure();
+}
+
+
+
+
+
