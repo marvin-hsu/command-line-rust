c22d44e430e66b3018b7a104b56c096489b6e7cb
Marvin Hsu <44745544+marvin-hsu@users.noreply.github.com>
2023-04-21 16:13:49 +0000

    pass all test

--- 02_echor/tests/cli.rs
+++ 02_echor/tests/cli.rs
@@ -3,23 +3,45 @@ use std::fs;
 use assert_cmd::Command;
 
 #[test]
-fn dies_no_args() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+fn dies_no_args() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.assert()
         .failure()
         .stderr(predicates::str::is_match(r"(?i)usAge").unwrap());
+
+    Ok(())
 }
 
 #[test]
-fn runs() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+fn runs() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.arg("hello").assert().success();
+    Ok(())
 }
 
 #[test]
-fn hello1(){
-    let outfile = "tests/expected/hello1.txt";
-    let expected = fs::read_to_string(outfile).unwrap();
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.arg("Hello there").assert().success().stdout(expected);
+fn hello1() -> Result<(), Box<dyn std::error::Error>> {
+    run(&vec!["Hello there"], "tests/expected/hello1.txt")
+}
+
+#[test]
+fn hello2() -> Result<(), Box<dyn std::error::Error>> {
+    run(&vec!["Hello", "there"], "tests/expected/hello2.txt")
+}
+
+#[test]
+fn hello1_no_newline() -> Result<(), Box<dyn std::error::Error>> {
+    run(&vec!["Hello  there", "-n"], "tests/expected/hello1.n.txt")
+}
+
+#[test]
+fn hello2_no_newline() -> Result<(), Box<dyn std::error::Error>> {
+    run(&vec!["-n", "Hello", "there"], "tests/expected/hello2.n.txt")
+}
+
+fn run(args: &[&str], expected_file: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let expected = fs::read_to_string(expected_file)?;
+    let mut cmd = Command::cargo_bin("echor")?;
+    cmd.args(args).assert().success().stdout(expected);
+    Ok(())
 }
