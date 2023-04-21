24657041505804ba87ace5bb0e34d13b17752eb8
Marvin Hsu <44745544+marvin-hsu@users.noreply.github.com>
2023-04-21 15:47:04 +0000

    compare with raw echo

--- 02_echor/tests/cli.rs
+++ 02_echor/tests/cli.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 use assert_cmd::Command;
 
 #[test]
@@ -12,4 +14,12 @@ fn dies_no_args() {
 fn runs() {
     let mut cmd = Command::cargo_bin("echor").unwrap();
     cmd.arg("hello").assert().success();
+}
+
+#[test]
+fn hello1(){
+    let outfile = "tests/expected/hello1.txt";
+    let expected = fs::read_to_string(outfile).unwrap();
+    let mut cmd = Command::cargo_bin("echor").unwrap();
+    cmd.arg("Hello there").assert().success().stdout(expected);
 }
