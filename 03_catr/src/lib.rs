59a1d10a7f6f1661ef8d110af9e66998cdc3b9a4
Marvin Hsu <44745544+marvin-hsu@users.noreply.github.com>
2023-04-22 08:18:40 +0000

    test fail

--- 03_catr/src/lib.rs
+++ 03_catr/src/lib.rs
@@ -1,1 +1,54 @@
-type MyResult<T> = Result<T, Box<dyn Err>>;
+use clap::{Arg, ArgAction, Command};
+use std::error::Error;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    number_lines: bool,
+    number_nonblank_lines: bool,
+}
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+pub fn run(config: Config) -> MyResult<()> {
+    dbg!(config);
+    Ok(())
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = Command::new("catr")
+        .version("0.1.0")
+        .author("marvinhsu")
+        .about("Rust cat")
+        .arg(
+            Arg::new("files")
+                .value_name("FILE")
+                .help("Input files(s)")
+                // 取代multiple方法
+                .action(ArgAction::Append)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::new("number")
+                .short('n')
+                .long("number")
+                .help("Number lines")
+                .action(ArgAction::SetTrue)
+                .conflicts_with("number_nonblank"),
+        )
+        .arg(
+            Arg::new("number_nonblank")
+                .short('b')
+                .long("number-nonblank")
+                .help("Number non-blank lines")
+                .action(ArgAction::SetTrue)
+                .conflicts_with("number_nonblank"),
+        )
+        .get_matches();
+
+    Ok(Config {
+        files: matches.get_many::<String>("files").unwrap().map(|s|s.clone()).collect(),
+        number_lines: matches.get_flag("number"),
+        number_nonblank_lines: matches.get_flag("number_nonblank"),
+    })
+}
--- 03_catr/src/main.rs
+++ 03_catr/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = catr::get_args().and_then(catr::run) {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
 }
