cebf011cf7db4d19f6bff0ce1ebe5c3838b49ed0
Marvin Hsu <44745544+marvin-hsu@users.noreply.github.com>
2023-04-22 07:27:33 +0000

    cargo fmt

--- 02_echor/src/main.rs
+++ 02_echor/src/main.rs
@@ -1,4 +1,4 @@
-use clap::{Arg, Command, ArgAction};
+use clap::{Arg, ArgAction, Command};
 
 fn main() {
     let matches = Command::new("echor")
@@ -10,12 +10,14 @@ fn main() {
                 .value_name("TEXT")
                 .help("Input text")
                 .required(true)
+                // clap4後 參數數量統一使用num_args
                 .num_args(1..),
         )
         .arg(
             Arg::new("omit_newline")
                 .short('n')
                 .help("Do not print newline")
+                // clap4後對於無參數命令可轉用action設定Arg
                 .action(ArgAction::SetTrue),
         )
         .get_matches();
@@ -25,6 +27,11 @@ fn main() {
         .unwrap()
         .map(|s| s.as_str())
         .collect();
+
+    // clap4將is_present改為contains_id
+    // 有預設值情況下contains_id接回傳true
+    // 如果設定num_args(0)也會回傳true
+    // 改用ArgAction
     let omit_newline = matches.get_flag("omit_newline");
 
     print!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" });
