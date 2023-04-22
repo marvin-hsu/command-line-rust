6ee9ec1606a67b27e23fa83f1396d34340a2541d
Marvin Hsu <44745544+marvin-hsu@users.noreply.github.com>
2023-04-22 11:29:24 +0000

    finish feature

--- 03_catr/src/lib.rs
+++ 03_catr/src/lib.rs
@@ -1,5 +1,9 @@
 use clap::{Arg, ArgAction, Command};
-use std::error::Error;
+use std::{
+    error::Error,
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
 
 #[derive(Debug)]
 pub struct Config {
@@ -11,8 +15,37 @@ pub struct Config {
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
 pub fn run(config: Config) -> MyResult<()> {
-    dbg!(config);
+    for filename in config.files {
+        match open(&filename) {
+            Err(err) => eprintln!("Fail to open {} : {}", filename, err),
+            Ok(file) => {
+                let mut last_num = 0;
+                for (line_num, line) in file.lines().enumerate() {
+                    let line = line?;
+                    if config.number_lines {
+                        println!("{:>6}\t{}", line_num + 1, line);
+                    } else if config.number_nonblank_lines {
+                        if !line.is_empty() {
+                            last_num += 1;
+                            println!("{:>6}\t{}", line_num + 1, line);
+                        } else {
+                            println!();
+                        }
+                    } else {
+                        println!("{}", line);
+                    }
+                }
+            }
+        }
+    }
     Ok(())
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }
 
 pub fn get_args() -> MyResult<Config> {
@@ -41,13 +74,16 @@ pub fn get_args() -> MyResult<Config> {
                 .short('b')
                 .long("number-nonblank")
                 .help("Number non-blank lines")
-                .action(ArgAction::SetTrue)
-                .conflicts_with("number_nonblank"),
+                .action(ArgAction::SetTrue),
         )
         .get_matches();
 
     Ok(Config {
-        files: matches.get_many::<String>("files").unwrap().map(|s|s.clone()).collect(),
+        files: matches
+            .get_many::<String>("files")
+            .unwrap()
+            .map(|s| s.clone())
+            .collect(),
         number_lines: matches.get_flag("number"),
         number_nonblank_lines: matches.get_flag("number_nonblank"),
     })
