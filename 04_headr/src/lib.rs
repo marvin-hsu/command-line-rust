88a107be5d2fa445ff910d199bcef9852bb20ecd
Marvin Hsu <44745544+marvin-hsu@users.noreply.github.com>
2023-04-23 12:53:16 +0000

    impl read file

--- 04_headr/src/lib.rs
+++ 04_headr/src/lib.rs
@@ -1,3 +1,11 @@
+use std::{
+    error::Error,
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
+
+use clap::{Arg, ArgAction, Command};
+
 #[derive(Debug)]
 pub struct Config {
     files: Vec<String>,
@@ -7,17 +15,97 @@ pub struct Config {
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
-fn get_args() -> MyResult<Config> {
+pub fn get_args() -> MyResult<Config> {
     let matches = Command::new("headr")
         .version("0.1.0")
         .author("marvinhsu")
         .about("Rust head")
+        .arg(
+            Arg::new("lines")
+                .short('n')
+                .long("lines")
+                .value_name("LINES")
+                .help("Number of Lines")
+                .num_args(0..=1)
+                .default_value("10")
+                .default_missing_value("10"),
+        )
+        .arg(
+            Arg::new("bytes")
+                .short('c')
+                .long("bytes")
+                .value_name("BYTES")
+                .conflicts_with("lines")
+                .help("Number of Line")
+                .num_args(1),
+        )
+        .arg(
+            Arg::new("files")
+                .value_name("FILE")
+                .help("Input file(s")
+                .default_value("-")
+                .action(ArgAction::Append),
+        )
         .get_matches();
 
-    Ok(Config {});
+    let lines = matches
+        .get_one::<String>("lines")
+        .map(|x| parse_positive_int(x.as_str()))
+        .transpose()
+        .map_err(|e| format!("illegal line count -- {}", e))?;
+
+    let bytes = matches
+        .get_one::<String>("bytes")
+        .map(|x| parse_positive_int(x.as_str()))
+        .transpose()
+        .map_err(|e| format!("illegal line count -- {}", e))?;
+
+    Ok(Config {
+        files: matches
+            .get_many::<String>("files")
+            .unwrap()
+            .cloned()
+            .collect(),
+        lines: lines.unwrap(),
+        bytes,
+    })
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:#?}", config);
+    for filename in config.files {
+        match open(&filename) {
+            Err(e) => eprintln!("{}: {}", filename, e),
+            Ok(_) => println!("Opened {}", filename),
+        }
+    }
     Ok(())
+}
+
+fn parse_positive_int(val: &str) -> MyResult<usize> {
+    match val.parse() {
+        Ok(n) if n > 0 => Ok(n),
+        _ => Err(From::from(val)),
+    }
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?)))
+    }
+}
+
+#[test]
+fn test_parse_positive_int() {
+    let res = parse_positive_int("3");
+    assert!(res.is_ok());
+    assert_eq!(res.unwrap(), 3);
+
+    let res = parse_positive_int("foo");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "foo".to_string());
+
+    let res = parse_positive_int("0");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "0".to_string());
 }
