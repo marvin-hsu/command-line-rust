37b1ee7cafdf581e119f4d10d6af068ce230bdcd
Marvin Hsu <44745544+marvin-hsu@users.noreply.github.com>
2023-04-23 14:02:40 +0000

    refactor read bytes

--- 04_headr/src/lib.rs
+++ 04_headr/src/lib.rs
@@ -77,10 +77,12 @@ pub fn run(config: Config) -> MyResult<()> {
             Err(e) => eprintln!("{}: {}", filename, e),
             Ok(mut file) => {
                 if let Some(num_bytes) = config.bytes {
-                    let mut handle = file.take(num_bytes as u64);
-                    let mut buffer = vec![0; num_bytes];
-                    let bytes_read = handle.read(&mut buffer)?;
-                    print!("{}", String::from_utf8_lossy(&buffer[..bytes_read]));
+                    // let mut handle = file.take(num_bytes as u64);
+                    // let mut buffer = vec![0; num_bytes];
+                    // let bytes_read = handle.read(&mut buffer)?;
+                    // print!("{}", String::from_utf8_lossy(&buffer[..bytes_read]));
+                    let bytes : Result<Vec<_>,_> = file.bytes().take(num_bytes).collect();
+                    print!("{}",String::from_utf8_lossy(&bytes?));
                 } else {
                     let mut line = String::new();
                     for _ in 0..config.lines {
