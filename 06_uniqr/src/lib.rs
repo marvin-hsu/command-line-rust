86695504f4d19e3fbf19e9efd4ddd14bb8748eab
Marvin Hsu <44745544+marvin-hsu@users.noreply.github.com>
2023-04-26 14:38:03 +0000

    parse args

--- 06_uniqr/src/lib.rs
+++ 06_uniqr/src/lib.rs
@@ -1,4 +1,4 @@
-use clap::Command;
+use clap::{Arg, ArgAction, Command};
 use std::error::Error;
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
@@ -15,12 +15,31 @@ pub fn get_args() -> MyResult<Config> {
         .version("0.1.0")
         .author("marvinhsu")
         .about("Rust uniq")
+        .arg(
+            Arg::new("count")
+                .short('c')
+                .long("count")
+                .help("Show counts")
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("in_file")
+                .value_name("IN_FILE")
+                .help("Input file")
+                .default_value("-"),
+        )
+        .arg(
+            Arg::new("out_file")
+                .value_name("OUT_FILE")
+                .help("Output file")
+                .num_args(0..=1),
+        )
         .get_matches();
 
     Ok(Config {
-        in_files: "".to_string(),
-        out_files: None,
-        count: false,
+        in_files: matches.get_one::<String>("in_file").unwrap().clone(),
+        out_files: matches.get_one::<String>("out_file").cloned(),
+        count: matches.get_flag("count"),
     })
 }
 
