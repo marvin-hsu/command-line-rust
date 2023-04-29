c497e5e708e50ed01bb9ab9853b8e8bacf864dfa
Marvin Hsu <44745544+marvin-hsu@users.noreply.github.com>
2023-04-29 09:04:41 +0000

    prepare

--- 08_cutr/src/lib.rs
+++ 08_cutr/src/lib.rs
@@ -1,14 +1,75 @@
-use std::error::Error;
+use clap::{Arg, ArgAction, Command};
+use std::{error::Error, ops::Range};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
+type PositionList = Vec<Range<usize>>;
 
 #[derive(Debug)]
-pub struct Config {}
+pub struct Config {
+    files: Vec<String>,
+    delimiter: u8,
+    extract: Extract,
+}
+
+#[derive(Debug)]
+pub enum Extract {
+    Fields(PositionList),
+    Bytes(PositionList),
+    Chars(PositionList),
+}
 
 pub fn get_args() -> MyResult<Config> {
-    Ok(Config {})
+    let matches = Command::new("cutr")
+        .version("1.0.1")
+        .author("marvinhsu")
+        .about("Rust cut")
+        .arg(
+            Arg::new("files")
+                .default_value("-")
+                .action(ArgAction::Append)
+                .value_name("FILE")
+                .help("Input file(s)")
+                .num_args(0..),
+        )
+        .arg(
+            Arg::new("bytes")
+                .value_name("BYTES")
+                .short('b')
+                .long("bytes")
+                .help("Selected bytes"),
+        )
+        .arg(
+            Arg::new("chars")
+                .value_name("CHARS")
+                .short('c')
+                .long("chars")
+                .help("Selected characters"),
+        )
+        .arg(
+            Arg::new("Delim")
+                .value_name("DELIMITER")
+                .short('d')
+                .long("delim")
+                .default_value("")
+                .help("Field delimiter"),
+        )
+        .arg(
+            Arg::new("fields")
+                .value_name("FIELDS")
+                .short('f')
+                .long("fields")
+                .help("Selected fields"),
+        )
+        .get_matches();
+
+    Ok(Config {
+        files: todo!(),
+        delimiter: todo!(),
+        extract: todo!(),
+    })
 }
 
 pub fn run(config: Config) -> MyResult<()> {
+    println!("{:?}", config);
     Ok(())
 }
