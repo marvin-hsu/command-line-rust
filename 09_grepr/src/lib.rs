c251661c74addd657d09334bf8f870724dbebb85
Marvin Hsu <44745544+marvin-hsu@users.noreply.github.com>
2023-04-30 09:29:54 +0000

    parse args

--- 09_grepr/src/lib.rs
+++ 09_grepr/src/lib.rs
@@ -1,3 +1,4 @@
+use clap::{Arg, ArgAction, Command};
 use std::error::Error;
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
@@ -10,9 +11,51 @@ pub fn get_args() -> MyResult<Config> {
         .version("0.1.0")
         .author("marvinhsu")
         .about("Rust grep")
+        .arg(
+            Arg::new("pattern")
+                .value_name("PATTERN")
+                .help("Search pattern")
+                .required(true),
+        )
+        .arg(
+            Arg::new("file")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .default_value("-")
+                .num_args(0..)
+                .action(ArgAction::Append),
+        )
+        .arg(
+            Arg::new("count")
+                .short('c')
+                .long("count")
+                .help("Count occurrences")
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("insensitive")
+                .short('i')
+                .long("insensitive")
+                .help("Case-insensitive")
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("invert-match")
+                .short('v')
+                .long("invert-match")
+                .help("Invert match")
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("recursive")
+                .short('r')
+                .long("recursive")
+                .help("Recursive search")
+                .action(ArgAction::SetTrue),
+        )
         .get_matches();
 
-    Config {}
+    Ok(Config {})
 }
 
 pub fn run(config: Config) -> MyResult<()> {
