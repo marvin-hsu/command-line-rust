5d8cfea926fa41c96dd56f05f1548e8e3174f500
Marvin Hsu <44745544+marvin-hsu@users.noreply.github.com>
2023-04-30 09:18:10 +0000

    setting

--- 09_grepr/src/lib.rs
+++ 09_grepr/src/lib.rs
@@ -6,9 +6,16 @@ type MyResult<T> = Result<T, Box<dyn Error>>;
 pub struct Config {}
 
 pub fn get_args() -> MyResult<Config> {
-    todo!();
+    let matches = Command::new("grepr")
+        .version("0.1.0")
+        .author("marvinhsu")
+        .about("Rust grep")
+        .get_matches();
+
+    Config {}
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    todo!();
+    println!("{:#?}", config);
+    Ok(())
 }
