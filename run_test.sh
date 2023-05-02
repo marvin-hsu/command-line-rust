47be2712c6b3c4469c82652476530545eeef05b3
Marvin Hsu <44745544+marvin-hsu@users.noreply.github.com>
2023-05-02 14:37:26 +0000

    modify test pipline

--- run_test.sh
+++ run_test.sh
@@ -1,9 +1,6 @@
-cargo install cargo2junit
 for dir in [0-9][0-9]_*; do
     if [ -d "$dir" ]; then
         echo "Running tests for $dir"
-        (cd "$dir" && cargo test -- -Z unstable-options --format json --report-time | cargo2junit > results.xml)
+        (cd "$dir" && cargo test)
     fi
 done
-
-find . -name 'results.xml' -type f -print0 | xargs -0 cat > combined_results.xml
