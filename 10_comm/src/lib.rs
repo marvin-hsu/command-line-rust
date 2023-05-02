636dc2c898f89319f8b962ccfc85ac962fc9c256
Marvin Hsu <44745544+marvin-hsu@users.noreply.github.com>
2023-05-02 15:06:08 +0000

    pass die tests

--- 10_comm/src/lib.rs
+++ 10_comm/src/lib.rs
@@ -1,4 +1,8 @@
-use std::error::Error;
+use std::{
+    error::Error,
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
 
 use clap::{Arg, ArgAction, Command};
 
@@ -79,6 +83,26 @@ pub fn get_args() -> MyResult<Config> {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:?}",config);
+    let file1 = &config.file1;
+    let file2 = &config.file2;
+
+    if file1 == "-" && file2 == "-" {
+        return Err(From::from("Both input files cannot be STDIN (\"-\")"));
+    }
+
+    let _file1 = open(file1)?;
+    let _file2 = open(file2)?;
+
+    println!("Opened {} and {}", file1, file2);
+
     Ok(())
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(
+            File::open(filename).map_err(|e| format!("{}: {}", filename, e))?,
+        ))),
+    }
 }
