a67b3f94db28949115c942896f5ca77ead0aa601
Marvin Hsu <44745544+marvin-hsu@users.noreply.github.com>
2023-05-02 13:58:17 +0000

    ci test with report

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,12 @@
+cargo install cargo2junit
 for dir in [0-9][0-9]_*; do
     if [ -d "$dir" ]; then
         echo "Running tests for $dir"
-        (cd "$dir" && cargo test)
+        (cd "$dir" && cargo test -- --format json | cargo2junit > results.xml)
     fi
 done
+
+cat results*.xml > combined_results.xml
+
+echo "::set-output name=combined_results_file::combined_results.xml"
+echo "::set-output name=combined_results_path::$(realpath combined_results.xml)"
