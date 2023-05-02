452769e11200fab925759b6f9bc7dc267747ad2a
Marvin Hsu <44745544+marvin-hsu@users.noreply.github.com>
2023-05-02 14:16:50 +0000

    modify

--- run_test.sh
+++ run_test.sh
@@ -1,12 +1,9 @@
-cargo install cargo2junit
+# cargo install cargo2junit
 for dir in [0-9][0-9]_*; do
     if [ -d "$dir" ]; then
         echo "Running tests for $dir"
-        (cd "$dir" && cargo test -- --format json | cargo2junit > results.xml)
+        (cd "$dir" && cargo test -- -Z unstable-options --format json --report-time | cargo2junit > results.xml)
     fi
 done
 
-cat results*.xml > combined_results.xml
-
-echo "::set-output name=combined_results_file::combined_results.xml"
-echo "::set-output name=combined_results_path::$(realpath combined_results.xml)"
+find . -name 'results.xml' -type f -print0 | xargs -0 cat > combined_results.xml
