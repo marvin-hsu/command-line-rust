362b4c7fc4da61470bdb36820a5dbb548253b7bf
Marvin Hsu <44745544+marvin-hsu@users.noreply.github.com>
2023-05-04 14:46:48 +0000

    setting args

--- 12_fortuner/src/lib.rs
+++ 12_fortuner/src/lib.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
 
-use clap::Command;
+use clap::{Arg, ArgAction, Command};
 use regex::Regex;
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
@@ -17,6 +17,34 @@ pub fn get_args() -> MyResult<Config> {
         .version("0.1.0")
         .author("marvinhsu")
         .about("Rust fortune")
+        .arg(
+            Arg::new("file")
+                .value_name("FILE")
+                .help("Input files or directories")
+                .action(ArgAction::Append)
+                .num_args(1..),
+        )
+        .arg(
+            Arg::new("insensitive")
+                .help("Case-insensitive pattern matching")
+                .action(ArgAction::SetTrue)
+                .short('i')
+                .long("insensitive"),
+        )
+        .arg(
+            Arg::new("pattern")
+                .help("Pattern")
+                .short('m')
+                .long("pattern")
+                .value_name("PATTERN"),
+        )
+        .arg(
+            Arg::new("seed")
+                .help("Random seed")
+                .short('s')
+                .long("seed")
+                .value_name("SEED"),
+        )
         .get_matches();
 
     Ok(Config {
