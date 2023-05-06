66d5839fe8aed5e339da19d775c44d422b65790e
Marvin Hsu <44745544+marvin-hsu@users.noreply.github.com>
2023-05-06 12:55:10 +0000

    pass calr tests

--- 13_calr/src/lib.rs
+++ 13_calr/src/lib.rs
@@ -1,23 +1,25 @@
 use std::{error::Error, str::FromStr};
 
+use ansi_term::Style;
 use chrono::{Datelike, Local, NaiveDate};
 use clap::{Arg, ArgAction, Command};
-use itertools::Itertools;
+use itertools::{izip, Itertools};
 
 const MONTH_NAMES: [&str; 12] = [
-    "january",
-    "gebruary",
-    "march",
-    "april",
-    "may",
-    "june",
-    "july",
-    "august",
-    "september",
-    "october",
-    "november",
-    "december",
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
 ];
+const LINE_WIDTH: usize = 22;
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -39,7 +41,7 @@ pub fn get_args() -> MyResult<Config> {
                 .long("year")
                 .help("Show whole current year")
                 .action(ArgAction::SetTrue)
-                .conflicts_with_all(&["month","year"]),
+                .conflicts_with_all(&["month", "year"]),
         )
         .arg(
             Arg::new("month")
@@ -47,12 +49,7 @@ pub fn get_args() -> MyResult<Config> {
                 .value_name("MONTH")
                 .help("Month name or number"),
         )
-        .arg(
-            Arg::new("year")
-                .value_name("YEAR")
-                .help("Year")
-                .num_args(1),
-        )
+        .arg(Arg::new("year").value_name("YEAR").help("Year").num_args(1))
         .get_matches();
 
     let mut month = matches
@@ -80,7 +77,31 @@ pub fn get_args() -> MyResult<Config> {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:?}", config);
+    match config.month {
+        Some(month) => {
+            let lines = format_month(config.year, month, true, config.today);
+            println!("{}", lines.join("\n"));
+        }
+        None => {
+            println!("{:>32}", config.year);
+            let months: Vec<_> = (1..=12)
+                .into_iter()
+                .map(|month| format_month(config.year, month, false, config.today))
+                .collect();
+
+            for (i, chunk) in months.chunks(3).enumerate() {
+                if let [m1, m2, m3] = chunk {
+                    for lines in izip!(m1, m2, m3) {
+                        println!("{}{}{}", lines.0, lines.1, lines.2);
+                    }
+                    if i < 3 {
+                        println!();
+                    }
+                }
+            }
+        }
+    }
+
     Ok(())
 }
 
@@ -113,16 +134,73 @@ pub fn parse_month(month: &str) -> MyResult<u32> {
             match MONTH_NAMES
                 .iter()
                 .enumerate()
-                .find(|v| v.1.starts_with(&lower))
+                .find(|v| v.1.to_lowercase().starts_with(&lower))
             {
-                Some(m) => {
-                    println!("{}", m.1);
-                    Ok(m.0 as u32 + 1)
-                }
+                Some(m) => Ok(m.0 as u32 + 1),
                 None => Err(format!("Invalid month \"{}\"", month).into()),
             }
         }
     }
+}
+
+fn format_month(year: i32, month: u32, print_year: bool, today: NaiveDate) -> Vec<String> {
+    let first = NaiveDate::from_ymd_opt(year, month, 1).unwrap();
+    let mut days: Vec<String> = (1..first.weekday().number_from_sunday())
+        .into_iter()
+        .map(|_| "  ".to_string()) // two spaces
+        .collect();
+    let is_today = |day: u32| year == today.year() && month == today.month() && day == today.day();
+
+    let last = last_day_in_month(year, month);
+    days.extend((first.day()..=last.day()).into_iter().map(|num| {
+        let fmt = format!("{:>2}", num);
+        if is_today(num) {
+            Style::new().reverse().paint(fmt).to_string()
+        } else {
+            fmt
+        }
+    }));
+
+    let month_name = MONTH_NAMES[month as usize - 1];
+    let mut lines = Vec::with_capacity(8);
+    lines.push(format!(
+        "{:^20}  ", // two trailing spaces
+        if print_year {
+            format!("{} {}", month_name, year)
+        } else {
+            month_name.to_string()
+        }
+    ));
+
+    lines.push("Su Mo Tu We Th Fr Sa  ".to_string()); // two trailing spaces
+
+    for week in days.chunks(7) {
+        lines.push(format!(
+            "{:width$}  ", // two trailing spaces
+            week.join(" "),
+            width = LINE_WIDTH - 2
+        ));
+    }
+
+    while lines.len() < 8 {
+        lines.push(" ".repeat(LINE_WIDTH));
+    }
+
+    lines
+}
+
+fn last_day_in_month(year: i32, month: u32) -> NaiveDate {
+    // The first day of the next month...
+    let (y, m) = if month == 12 {
+        (year + 1, 1)
+    } else {
+        (year, month + 1)
+    };
+    // ...is preceded by the last day of the original month
+    NaiveDate::from_ymd_opt(y, m, 1)
+        .unwrap()
+        .pred_opt()
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -207,5 +285,45 @@ mod tests {
         let res = parse_month("foo");
         assert!(res.is_err());
         assert_eq!(res.unwrap_err().to_string(), "Invalid month \"foo\"");
+    }
+    #[test]
+    fn test_format_month() {
+        let today = NaiveDate::from_ymd_opt(0, 1, 1).unwrap();
+        let leap_february = vec![
+            "   February 2020      ",
+            "Su Mo Tu We Th Fr Sa  ",
+            "                   1  ",
+            " 2  3  4  5  6  7  8  ",
+            " 9 10 11 12 13 14 15  ",
+            "16 17 18 19 20 21 22  ",
+            "23 24 25 26 27 28 29  ",
+            "                      ",
+        ];
+        assert_eq!(format_month(2020, 2, true, today), leap_february);
+
+        let may = vec![
+            "        May           ",
+            "Su Mo Tu We Th Fr Sa  ",
+            "                1  2  ",
+            " 3  4  5  6  7  8  9  ",
+            "10 11 12 13 14 15 16  ",
+            "17 18 19 20 21 22 23  ",
+            "24 25 26 27 28 29 30  ",
+            "31                    ",
+        ];
+        assert_eq!(format_month(2020, 5, false, today), may);
+
+        let april_hl = vec![
+            "     April 2021       ",
+            "Su Mo Tu We Th Fr Sa  ",
+            "             1  2  3  ",
+            " 4  5  6 \u{1b}[7m 7\u{1b}[0m  8  9 10  ",
+            "11 12 13 14 15 16 17  ",
+            "18 19 20 21 22 23 24  ",
+            "25 26 27 28 29 30     ",
+            "                      ",
+        ];
+        let today = NaiveDate::from_ymd_opt(2021, 4, 7).unwrap();
+        assert_eq!(format_month(2021, 4, true, today), april_hl);
     }
 }
